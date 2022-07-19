95f6ac662f1f795e8c2f90342c304ed44f3372c9
Leonardo Araujo de Paulo <leonardo.araujo300301@gmail.com>
2022-07-19 00:56:15 -0300

    requisito 3 feito

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+const pokedexService = require('../services/pokedexService');
+
+const pikachu = 25;
+const charmander = 4;
+
+afterEach((() => jest.clearAllMocks));
+
+describe('3- Testando o componente <FavoritePokemons.js />', () => {
+  test('Caso não tenha pokemons favoritos, aparecera: No favorite pokemon found', () => {
+    jest.spyOn(pokedexService, 'readFavoritePokemonIds').mockReturnValue([]);
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorites');
+    const text = screen.getByText('No favorite pokemon found');
+    expect(text).toBeInTheDocument();
+  });
+  test('Teste se são exibidos todos os cards de pokémons favoritados', () => {
+    pokedexService.readFavoritePokemonIds.mockReturnValue([pikachu, charmander]);
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorites');
+    const name1 = screen.getByText('Pikachu');
+    const name2 = screen.getByText('Charmander');
+    expect(name1).toBeInTheDocument();
+    expect(name2).toBeInTheDocument();
+  });
+});
